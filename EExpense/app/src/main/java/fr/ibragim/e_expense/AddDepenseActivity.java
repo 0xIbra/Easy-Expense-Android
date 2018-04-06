a5b51c4d33179228663c5b3c7fdf48d5fa88aa4e
Vincent <vincent.nicolau@live.fr>
2018-04-06 17:11:38 +0200

    MAJ 06/04/2018 17H11

--- EExpense/app/src/main/java/fr/ibragim/e_expense/AddDepenseActivity.java
+++ EExpense/app/src/main/java/fr/ibragim/e_expense/AddDepenseActivity.java
@@ -1,34 +1,45 @@
 package fr.ibragim.e_expense;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
+
+
+import java.io.File;
 
 public class AddDepenseActivity extends AppCompatActivity {
+
     private TextView Output;
     private Button changeDate;
-
     private int year;
     private int month;
     private int day;
 
-    static final int DATE_PICKER_ID = 1111;
-
     private ImageView addPicture;
+    static final int DATE_PICKER_ID = 1111;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
     private static String[] PERMISSIONS_STORAGE_CAMERA = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -37,14 +48,18 @@ public class AddDepenseActivity extends AppCompatActivity {
             Manifest.permission.CAPTURE_VIDEO_OUTPUT,
             Manifest.permission.CAPTURE_AUDIO_OUTPUT
     };
+    private Uri imageUri;
 
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    //@RequiresApi(api = Build.VERSION_CODES.N)
+    @TargetApi(Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_depense);
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.customToolbar);
+
+        toolbar.setTitle("Ajouter une dépense");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -56,9 +71,9 @@ public class AddDepenseActivity extends AppCompatActivity {
 
         // Get current date by calender
         final Calendar c = Calendar.getInstance();
-        year  = c.get(Calendar.YEAR);
+        year = c.get(Calendar.YEAR);
         month = c.get(Calendar.MONTH);
-        day   = c.get(Calendar.DAY_OF_MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
 
         // Show current date
         Output.setText(new StringBuilder()
@@ -75,20 +90,67 @@ public class AddDepenseActivity extends AppCompatActivity {
                 showDialog(DATE_PICKER_ID);
             }
         });
+        StrictMode.VmPolicy.Builder newbuilder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(newbuilder.build());
+        Log.v("Path image", this.getExternalFilesDir(Environment.DIRECTORY_DCIM).getAbsolutePath());
+        addPicture.setOnClickListener(new View.OnClickListener() {
 
-
-        addPicture.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Coucou",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Capture d'écran en cours", Toast.LENGTH_LONG).show();
+                int permission = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permission != PackageManager.PERMISSION_GRANTED) {
+                    // Nous n'avons pas la permission de stockée dans le
+                    // PackageManager pour READ et WRITE sur external storage + Utiliser LA CAMERA
+                    ActivityCompat.requestPermissions(
+                            AddDepenseActivity.this,
+                            PERMISSIONS_STORAGE_CAMERA,
+                            CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE
+                    );
+                } else {
+                    takePictureIntent();
+                }
+
             }
         });
     }
 
 
+    private void takePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Log.v("Path image", this.getExternalFilesDir(Environment.DIRECTORY_DCIM).getAbsolutePath());
+        imageUri = Uri.fromFile(new
+                File(this.getExternalFilesDir(Environment.DIRECTORY_DCIM), "justificatif_" +
+                String.valueOf(System.currentTimeMillis()) + ".jpg"));
+        takePictureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                retrieveCapturedPicture();
+            }
+        }
+    }
+
+    private void retrieveCapturedPicture() {
+        // display picture on ImageView or write path of image file into database SQLite
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bitmap = BitmapFactory.decodeFile(imageUri.getPath(), options);
+        addPicture.setImageBitmap(bitmap);
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
                 return true;
@@ -108,7 +170,7 @@ public class AddDepenseActivity extends AppCompatActivity {
                 // open datepicker dialog.
                 // set date picker for current date
                 // add pickerListener listner to date picker
-                return new DatePickerDialog(this, pickerListener, year, month,day);
+                return new DatePickerDialog(this, pickerListener, year, month, day);
         }
         return null;
     }
@@ -120,9 +182,9 @@ public class AddDepenseActivity extends AppCompatActivity {
         public void onDateSet(DatePicker view, int selectedYear,
                               int selectedMonth, int selectedDay) {
 
-            year  = selectedYear;
+            year = selectedYear;
             month = selectedMonth;
-            day   = selectedDay;
+            day = selectedDay;
 
             // Show selected date
             Output.setText(new StringBuilder().append(month + 1)
