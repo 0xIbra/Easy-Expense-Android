57c43a81e6f2615e6bd947bc475f93008aba56ea
Ibragim <ibragim.abubakarov@greta-sud-aquitaine.academy>
2018-04-07 09:12:49 +0200

    mise a jour 06/04/18 09:11
    Ajout de la fonction permettant de verifier si le telephone a bien un acces a internet

--- EExpense/app/src/main/java/fr/ibragim/e_expense/LoginActivity.java
+++ EExpense/app/src/main/java/fr/ibragim/e_expense/LoginActivity.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import fr.ibragim.e_expense.Session.UserSession;
+import fr.ibragim.e_expense.network.ConnectionDetector;
 import fr.ibragim.e_expense.network.HttpsPostRequest;
 
 public class LoginActivity extends AppCompatActivity {
@@ -37,37 +38,64 @@ public class LoginActivity extends AppCompatActivity {
     protected HttpsPostRequest getRequest;
     private int userID;
 
+    ConnectionDetector connectionDetector;
+
+
+    //Layout resources
+    EditText emailField;
+    EditText passField;
+    Button btn ;
+    Switch rememberSw ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         userPrefs = getSharedPreferences(this.USER_SESSION, MODE_PRIVATE);
         API_URL = "https://api.ibragim.fr/Android.php";
-        boolean SessionCheck = UserSession.CheckSession(userPrefs);
 
-        if (SessionCheck){
-            Map<String, ?> userS = UserSession.getUserSession(userPrefs);
-            String params;
-            String sharedToken = userS.get("USER_TOKEN").toString();
-            String sharedEmail = userS.get("USER_EMAIL").toString();
+        emailField = findViewById(R.id.loginField);
+        passField  = findViewById(R.id.passField);
+        btn = findViewById(R.id.btnconnect);
+        rememberSw = findViewById(R.id.rememberSwitch);
 
-            System.out.println("TOKEN " + sharedToken);
-            System.out.println("SHAREDEMAIL " + sharedEmail);
 
-            byte[] tokenBytes = sharedToken.getBytes();
+        SessionCheckAndConnect();
 
-            byte[] decoded = Base64.decode(tokenBytes, Base64.NO_WRAP);
-            String decodedTest2 = new String(decoded);
-            userEmail = decodedTest2.substring(0, sharedEmail.length());
-            String pass = decodedTest2.substring(sharedEmail.length() + 1, decodedTest2.length());
 
-            params = "AuthToken=true&token="+sharedToken+"&mail="+userEmail+"&password="+pass;
-            System.out.println("PARAMS "+params);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Authentification(view);
+            }
+        });
 
+    }
+
+
+
+    public void Authentification(View view){
+        connectionDetector = new ConnectionDetector(getApplicationContext());
+
+        if (connectionDetector.isConnected()){
+            int userid = 0;
             try {
                 getRequest = new HttpsPostRequest();
-                result = getRequest.execute(API_URL, params).get();
-                System.out.println("PASS "+result);
+                String base = emailField.getText().toString() + ":" + passField.getText().toString();
+                AuthHeader = Base64.encodeToString(base.getBytes(), Base64.NO_WRAP);
+
+                byte[] decoding = AuthHeader.getBytes();
+                String decoded = new String(decoding);
+                userEmail = decoded.substring(0, emailField.getText().length());
+
+
+                //byte[] test = AuthHeader.getBytes();
+                //String test2 = new String(test);
+                //byte[] decoded = Base64.decode(test2, Base64.NO_WRAP);
+                //String decodedTest2 = new String(decoded);
+                //userEmail = decodedTest2.substring(0, emailField.getText().length());
+                //String pass = decodedTest2.substring(emailField.getText().length() + 1, decodedTest2.length());
+                result = getRequest.execute(API_URL,"AuthToken=true&token="+AuthHeader+"&mail="+emailField.getText().toString() +"&password="+ passField.getText().toString()).get(); // Exécution du Thread pour connexion.
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {
@@ -76,48 +104,71 @@ public class LoginActivity extends AppCompatActivity {
 
             try {
                 JSONObject user = new JSONObject(result);
-                authToken = user.getString("AuthToken");
-                userID = user.getInt("id");
+                authToken = user.getString("AuthToken").toString();
+                userid = user.getInt("id");
+
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            if (sharedToken.equals(authToken)){
-                Intent reco = new Intent(getApplicationContext(), MainActivity.class);
-                reco.putExtra(USER_TOKEN, authToken);
-                reco.putExtra(USER_ID, userID);
-                startActivity(reco);
-                finish();
+
+            if (AuthHeader.equals(authToken)){
+                byte[] decoding = AuthHeader.getBytes();
+                String decoded = new String(decoding);
+                userEmail = decoded.substring(0, emailField.getText().length());
+
+                if (rememberSw.isChecked()){
+                    UserSession.setSharedPrefs(userPrefs, authToken, userEmail);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(USER_TOKEN, authToken);
+                    intent.putExtra(USER_ID, userid);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(USER_TOKEN, authToken);
+                    intent.putExtra(USER_ID, userid);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }else{
+                Snackbar.make(view, "Email ou Mot de passe incorrects", Snackbar.LENGTH_SHORT).show();
             }
 
+        }else{
+            Snackbar.make(view, "Aucune connexion internet détectée", Snackbar.LENGTH_SHORT).show();
         }
-
-        final EditText emailField = findViewById(R.id.loginField);
-        final EditText passField = findViewById(R.id.passField);
-        Button btn = findViewById(R.id.btnconnect);
-        final Switch rememberSw = findViewById(R.id.rememberSwitch);
+    }
 
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int userid = 0;
+    public void SessionCheckAndConnect(){
+        boolean SessionCheck = UserSession.CheckSession(userPrefs);
+        if (SessionCheck){
+            connectionDetector = new ConnectionDetector(getApplicationContext());
+            if (connectionDetector.isConnected()){
+                Map<String, ?> userS = UserSession.getUserSession(userPrefs);
+                String params;
+                String sharedToken = userS.get("USER_TOKEN").toString();
+                String sharedEmail = userS.get("USER_EMAIL").toString();
+
+                System.out.println("TOKEN " + sharedToken);
+                System.out.println("SHAREDEMAIL " + sharedEmail);
+
+                byte[] tokenBytes = sharedToken.getBytes();
+
+                byte[] decoded = Base64.decode(tokenBytes, Base64.NO_WRAP);
+                String decodedTest2 = new String(decoded);
+                userEmail = decodedTest2.substring(0, sharedEmail.length());
+                String pass = decodedTest2.substring(sharedEmail.length() + 1, decodedTest2.length());
+
+                params = "AuthToken=true&token="+sharedToken+"&mail="+userEmail+"&password="+pass;
+                System.out.println("PARAMS "+params);
+
                 try {
                     getRequest = new HttpsPostRequest();
-                    String base = emailField.getText().toString() + ":" + passField.getText().toString();
-                    AuthHeader = Base64.encodeToString(base.getBytes(), Base64.NO_WRAP);
-
-                    byte[] decoding = AuthHeader.getBytes();
-                    String decoded = new String(decoding);
-                    userEmail = decoded.substring(0, emailField.getText().length());
-
-
-                    //byte[] test = AuthHeader.getBytes();
-                    //String test2 = new String(test);
-                    //byte[] decoded = Base64.decode(test2, Base64.NO_WRAP);
-                    //String decodedTest2 = new String(decoded);
-                    //userEmail = decodedTest2.substring(0, emailField.getText().length());
-                    //String pass = decodedTest2.substring(emailField.getText().length() + 1, decodedTest2.length());
-                    result = getRequest.execute(API_URL,"AuthToken=true&token="+AuthHeader+"&mail="+emailField.getText().toString() +"&password="+ passField.getText().toString()).get(); // Exécution du Thread pour connexion.
+                    result = getRequest.execute(API_URL, params).get();
+                    System.out.println("PASS "+result);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (ExecutionException e) {
@@ -126,41 +177,25 @@ public class LoginActivity extends AppCompatActivity {
 
                 try {
                     JSONObject user = new JSONObject(result);
-                    authToken = user.getString("AuthToken").toString();
-                    userid = user.getInt("id");
-
-
+                    authToken = user.getString("AuthToken");
+                    userID = user.getInt("id");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-                if (AuthHeader.equals(authToken)){
-                    byte[] decoding = AuthHeader.getBytes();
-                    String decoded = new String(decoding);
-                    userEmail = decoded.substring(0, emailField.getText().length());
-
-                    if (rememberSw.isChecked()){
-                        UserSession.setSharedPrefs(userPrefs, authToken, userEmail);
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        intent.putExtra(USER_TOKEN, authToken);
-                        intent.putExtra(USER_ID, userid);
-                        startActivity(intent);
-                        finish();
-                    }else {
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        intent.putExtra(USER_TOKEN, authToken);
-                        intent.putExtra(USER_ID, userid);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }else{
-                    Snackbar.make(view, "Email ou Mot de passe incorrects", Snackbar.LENGTH_SHORT).show();
+                if (sharedToken.equals(authToken)){
+                    Intent reco = new Intent(getApplicationContext(), MainActivity.class);
+                    reco.putExtra(USER_TOKEN, authToken);
+                    reco.putExtra(USER_ID, userID);
+                    startActivity(reco);
+                    finish();
                 }
 
-
+            }else{
+                Toast.makeText(getApplicationContext(), "Aucune connexion internet détectée", Toast.LENGTH_SHORT).show();
             }
-        });
 
+
+        }
     }
+
 }
