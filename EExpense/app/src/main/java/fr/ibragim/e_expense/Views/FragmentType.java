624c5d4ce77101cb4f7d8576bde10d33476c1819
Ibragim <ibragim.abubakarov@greta-sud-aquitaine.academy>
2018-04-08 22:23:45 +0200

    mise a jour 08/04/18 22:23

---
+++ EExpense/app/src/main/java/fr/ibragim/e_expense/Views/FragmentType.java
@@ -0,0 +1,13 @@
+package fr.ibragim.e_expense.Views;
+
+/**
+ * Created by ibragim.abubakarov on 08/04/2018.
+ */
+
+public interface FragmentType {
+    int FraisFragment = 1;
+    int TrajetFragment = 2;
+
+    int getFragmentType();
+
+}
