ae5fe58f763bc77ccda0ab4dbae1585518af149d
Vincent <vincent.nicolau@live.fr>
2018-04-12 14:49:50 +0200

    MAJ 12/04/2018

---
+++ EExpense/app/src/main/java/fr/ibragim/e_expense/network/HttpsPutRequest.java
@@ -0,0 +1,69 @@
+package fr.ibragim.e_expense.network;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by ibragim.abubakarov on 15/03/2018.
+ */
+
+public class HttpsPutRequest extends AsyncTask<String, Void, String> {
+    public static final String REQUEST_METHOD = "PUT";
+    public static final int READ_TIMEOUT = 15000;
+    public static final int CONNECTION_TIMEOUT = 15000;
+    private HttpsURLConnection conn;
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        String targetUrl = strings[0];
+        String parametres = strings[1];
+        URL url;
+        HttpsURLConnection conn = null;
+        try{
+            url = new URL(targetUrl);
+            conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod(REQUEST_METHOD);
+            conn.setReadTimeout(READ_TIMEOUT);
+            conn.setConnectTimeout(CONNECTION_TIMEOUT);
+
+            conn.setUseCaches(true);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(parametres);
+            wr.flush();
+            wr.close();
+
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while((line = reader.readLine()) != null){
+                response.append(line);
+                response.append('\r');
+            }
+            reader.close();
+
+            return response.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
